a4f1dc1acf7e344951cbd8fbc32c5301b3aff6e2
A-karkaih <akarkaih@gmail.com>
2023-12-27 19:37:41 +0100

    some ui modif

--- src/App.jsx
+++ src/App.jsx
@@ -34,24 +34,30 @@ function App() {
 
   return (
     <div className=" w-full h-screen flex justify-center items-center">
-      <div className=" flex flex-col items-center     w-[80%] rounded-md shadow-lg sm:w-[500px] h-[300px] bg-[#313a49]">
+      <div className=" flex flex-col items-center w-[80%] rounded-3xl shadow-lg sm:w-[700px] h-[400px] bg-[#313a49]">
         <span className="cursor-default mt-3 mb-8 text-[12px] text-green-600">
-          Advice {advice?.id}
+          Advice #{advice?.id}
         </span>
 
-        <div className="border-[1px] mb-5 flex justify-center items-center px-4 text-center  border-white rounded-md w-[80%] h-[40%] ">
-          <p className="text-sm cursor-default text-white">{advice?.advice}</p>
+        <div className="border-[1px] mb-5 flex justify-center items-center px-4 text-center  border-white rounded-md w-[80%] h-[50%] ">
+          <p className="text-2xl cursor-default text-white">
+            "{advice?.advice}"
+          </p>
         </div>
 
-        <div className="w-[80%]  ">
-          <img src="images/pattern-divider-desktop.svg" alt="pattern-divider" />
+        <div className="w-[80%]   ">
+          <img
+            className="w-full"
+            src="images/pattern-divider-desktop.svg"
+            alt="pattern-divider"
+          />
         </div>
 
         <div
           onClick={generateAdvice}
-          className="relative cursor-pointer hover:bg-slate-500 -bottom-[65px]  h-10 w-10 rounded-full flex justify-center items-center bg-green-400 "
+          className="relative cursor-pointer -bottom-[80px] hover:bg-slate-500 sm:-bottom-[70px]  h-[50px] w-[50px] rounded-full flex justify-center items-center bg-green-400 "
         >
-          <img className="w-4 h-4" src="images/icon-dice.svg" alt="icon-dice" />
+          <img className="w-6 h-6" src="images/icon-dice.svg" alt="icon-dice" />
         </div>
       </div>
     </div>
